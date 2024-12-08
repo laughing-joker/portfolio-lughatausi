9ac5f4f40a1afe6e5470d35f058143811ce1a39b
laughing-joker <76960131+laughing-joker@users.noreply.github.com>
2024-12-08 07:57:25 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ Personal-Portfolio-using-React-Tailwind-CSS/src/constants/index.js
@@ -0,0 +1,50 @@
+import project3 from "../assets/images/Logo.png";
+
+export const HERO_CONTENT = `As the founder of Lugha Tausi, I, Masako, am committed to fostering linguistic innovation and bridging cultures through technology. 
+My vision is to create solutions that not only connect people but also celebrate and preserve diverse languages, ensuring they thrive in the digital age. 
+Through Lugha Tausi, I aim to make a lasting impact in the world of language and communication.`;
+
+export const ABOUT_TEXT = `I am Ngonyani, a skilled full-stack developer with extensive experience in Next.js. I specialize in building dynamic, scalable web applications that offer seamless user experiences. 
+My approach combines modern development practices with a keen focus on performance and innovation 
+to deliver high-quality solutions.`;
+
+export const ABOUT_ME = `have hands-on experience in data science, where I focus on extracting valuable insights from complex data sets to drive informed decision-making. 
+With expertise in machine learning, statistical analysis, and data visualization, 
+I apply advanced techniques to solve real-world problems and optimize processes, 
+ensuring data-driven solutions that add measurable value.`;
+
+export const ABOUT_MI = `I have extensive experience in both web and mobile development, specializing in building responsive, user-friendly applications. 
+With a strong foundation in modern technologies, I focus on delivering efficient, scalable solutions that meet the needs of users and businesses alike. 
+My work is driven by a passion for creating seamless digital experiences across platforms`;
+
+export const EXPERIENCES = [
+  {
+    year: "2024 - Present",
+    role: "Senior Full Stack Developer",
+    company: "LughaTausi.inc",
+    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "LughaTausi Programming Language",
+    image: project3,
+    description:
+      "LughaTausi Swahili Programming Language that has RestFul API ,  Validation and Security and it is a user Friendly Programming Language its syntax are easy to learn and understand.",
+    technologies: ["Python", "React Js", "Next Js", "RestFul API"],
+  },
+];
+
+export const CONTACT = {
+  address: "P.O.Box 443 , Dar Es Salaam , Tanzania",
+  phoneNo: "+255 769 298 518",
+  email: "christianmasako@gmail.com",
+};
+
+export const DEVELOPERS = {
+  address: "P.O.Box 883 , Dodoma , Tanzania",
+  phoneNo: "+255 677 786 957",
+  email: "johnmakweba843@gmail.com",
+};
